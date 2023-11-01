bb3a7d2d5565d030ae2aa40539a4f57c3b79b55d
uakubue <uakubue@gmail.com>
2023-11-01 22:14:37 +0100

    first commit

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,11 @@
+const DashboardPage = () => {
+    return (
+        <div>
+            <h3>This is a Dashboard</h3>
+        </div>
+    )
+}
+
+export default DashboardPage;
+
+
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+import {  UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+const setUpPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return null
+}
+
+export default setUpPage;
